19c4bc1b4f97fef4902673dfde98370b3758f4f5
chris maddock <chris.maddock@uhnm.nhs.uk>
2024-10-05 15:06:53 +0100

    pass loan object back to parent after save

--- Ventilation/Components/Shared/AddLoan/AddLoan.razor.cs
+++ Ventilation/Components/Shared/AddLoan/AddLoan.razor.cs
@@ -9,6 +9,10 @@ namespace Ventilation.Components.Shared.AddLoan
         [Parameter] public int PatientId { get; set; }
 
         StockItem? stockItemSelected = new();
+        Tabs tabs = default!;
+        bool patientLoanCreated = false;
+        Loan? loan = null;
+        List<ToastMessage> messages = new List<ToastMessage>();
 
         protected override void OnInitialized()
         {
@@ -26,6 +30,32 @@ namespace Ventilation.Components.Shared.AddLoan
             }
 
         }
+
+        //Event callback from child component after saving the loan form
+        protected async Task LoanIdChanged(Loan? Loan)
+        {
+            await Task.Delay(100);
+            if (Loan != null)
+            {
+                loan = Loan;
+                patientLoanCreated = true;
+                StateHasChanged();
+
+                ShowMessage(ToastType.Success);
+                await tabs.ShowTabByNameAsync("Prescriptions");
+            }
+
+        }
+
+
+        private void ShowMessage(ToastType toastType) => messages.Add(CreateSaveMessage(toastType));
+
+        private ToastMessage CreateSaveMessage(ToastType toastType)
+    => new ToastMessage
+    {
+        Type = toastType,
+        Message = $"Loan Successfully Created!",
+    };
 
     }
 }
--- Ventilation/Components/Shared/AddLoan/AddLoanForm.razor.cs
+++ Ventilation/Components/Shared/AddLoan/AddLoanForm.razor.cs
@@ -16,6 +16,10 @@ namespace Ventilation.Components.Shared.AddLoan
         [Inject]
         IPatientManager _patientManager { get; set; }
 
+        [Parameter]
+        public EventCallback<Loan?> OnLoanSaved { get; set; }
+
+
         public Loan? loanDetail = new();
 
         protected override void OnInitialized()
@@ -41,8 +45,21 @@ namespace Ventilation.Components.Shared.AddLoan
         private async Task OnSaveLoan(EditContext context)
         {
             int? Id = await _patientManager.SavePatientLoan((Loan)context.Model);
-            var yy = "";
-           // await OnPatientSaved.InvokeAsync(Id);
+
+            //TODO: need to pass the newly created Loan object back to the parent
+            //Options, 1. Just creat a new loan object here from the Model and add the Id, 2. return the loan object from the save 3. Do a get loan method after the save once we have the Id
+            Loan l = new Loan();
+            l.LoanId = Id;
+            l.LoanDate = loanDetail.LoanDate;
+            l.PatientId = patientId;
+            l.StockId = stockItem.StockId;
+            l.ClinicalReference = stockItem.ClinicalReference;
+            l.ServiceDate = stockItem.ServiceDate;
+            l.EquipmentName = stockItem.EquipmentName;
+            l.SerialNumber = stockItem.SerialNumber;
+            l.LoanDate = loanDetail.LoanDate;
+
+            await OnLoanSaved.InvokeAsync(l);
 
         }
 
