ac9e06fbd505c1375206ad16b1e02f812d80697c
chris maddock <chris.maddock@uhnm.nhs.uk>
2024-10-11 16:06:42 +0100

    consumables

--- Ventilation/Components/Shared/ConsumableComponents/ConsumableWrapper.razor.cs
+++ Ventilation/Components/Shared/ConsumableComponents/ConsumableWrapper.razor.cs
@@ -1,7 +1,42 @@
+using BAL.Managers;
+using Domain.Models;
+using Microsoft.AspNetCore.Components;
+
 namespace Ventilation.Components.Shared.ConsumableComponents
 {
     public partial class ConsumableWrapper
     {
+        [Parameter]
+        public Loan? paramLoan { get; set; }
 
+
+        public bool? AddConsumable { get; set; }
+
+        [Inject]
+        ILoanManager _loanManager { get; set; }
+        List<Consumable> loanConsumables = new();
+
+        public bool? UserClickedAdd { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            loanConsumables = null;
+            UserClickedAdd = false;
+
+            var xx = paramLoan;
+            if (paramLoan != null)
+            {
+                if (loanConsumables == null)
+                {
+                    loanConsumables = await _loanManager.GetConsumablesForALoan(paramLoan.LoanId);
+                }
+            }
+        }
+
+        private async Task OnAddConsumableClick()
+        {
+            await Task.Delay(100);
+            UserClickedAdd = true;
+        }
     }
 }
