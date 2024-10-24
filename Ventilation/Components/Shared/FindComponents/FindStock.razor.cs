4a1e92250231832ae874f1db809c5c5eb361149b
chris maddock <chris.maddock@uhnm.nhs.uk>
2024-10-24 14:37:30 +0100

    remove prescriptionItem

--- Ventilation/Components/Shared/FindComponents/FindStock.razor.cs
+++ Ventilation/Components/Shared/FindComponents/FindStock.razor.cs
@@ -8,43 +8,16 @@ namespace Ventilation.Components.Shared.FindComponents
 {
     public partial class FindStock
     {
-        //[Inject]
-        //IStockManager _stockManager { get; set; }
-
-     //   public StockToFind? stockToFind = new();
-      //  List<StockItem> stockitemsFound = new();
-
-        public bool? RefreshList { get; set; }
-     
-        
-        //TODO: remove redundant code if Simon / Vent team are happy with the grid
-
+       public bool? RefreshList { get; set; }
+      
         [Parameter]
         public EventCallback<StockItem?> OnStockSelected { get; set; }
-
-        //IQueryable<StockItem>? stockList;
-
-        // PaginationState state = new PaginationState { ItemsPerPage = 5 };
 
         protected override async Task OnInitializedAsync()
         {
             RefreshList = false;
         }
-
-
-        //protected async Task OnFindClick(EditContext context)
-        //{
-        //    stockitemsFound = await _stockManager.FindStock(((StockToFind)context.Model).ClinicalTechRef);
-        //    stockList = stockitemsFound.AsQueryable();
-
-
-        //}
-
-        //public class StockToFind
-        //{
-        //    public string? ClinicalTechRef { get; set; } = string.Empty;
-        //}
-
+     
         //Event callback from child component after saving additional information
         protected async Task StockSelected(StockItem? stock)
         {
@@ -55,14 +28,7 @@ namespace Ventilation.Components.Shared.FindComponents
             }
 
         }
-
-
-        //private async Task SelectStockClick(StockItem item)
-        //{
-
-
-        //    await OnStockSelected.InvokeAsync(item);
-        //}
+    
     }
 
 
